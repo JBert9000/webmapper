52d7fd8481d5718b8c29c8e7884d49807409341e
John <jbertlik@gmail.com>
2019-05-08 15:50:27 -0400

    I forgot to commit until now. Whoops. Well I am almost done this lesson. I have imported folium, and am getting to know the syntax of this useful library. This is neat, and I am enjoying learning about this.

---
+++ README.rd
@@ -0,0 +1,3 @@
+I am following one of the tutorials from this course: https://www.udemy.com/the-python-mega-course/
+
+Here I am making a webmapper using Python and the Folium library.
---
+++ webmapper.py
@@ -0,0 +1,32 @@
+import folium
+import pandas
+
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+# location = list(data["LOCATION"])
+# type = list(data["TYPE"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+map = folium.Map(location=[39.528039, -119.826600], zoom_start=7, tiles="Mapbox Bright")
+
+fg = folium.FeatureGroup(name="My Map")
+
+
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+"m",
+        fill_color=color_producer(
+        el), color="grey", fill=True, fill_opacity=0.7))
+
+map.add_child(fg)
+
+map.save("Map1.html")
